12b29e244ba81da35d12b5d6c26c455e978d4ef2
Tangshengku <981373223@qq.com>
2026-04-13 08:37:44 +0200

    misc

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -13,7 +13,7 @@ dev = torch.device("cuda")
 
 def get_model_from_huggingface(model_id):
     from transformers import AutoModelForCausalLM, LlamaTokenizer, AutoTokenizer, LlamaForCausalLM
-    if "opt" in model_id or "mistral" in model_id:
+    if "opt" in model_id or "mistral" in model_id or "Qwen" in model_id:
         tokenizer = AutoTokenizer.from_pretrained(model_id, device_map="auto", trust_remote_code=True, use_fast=False)
     else:
         tokenizer = LlamaTokenizer.from_pretrained(model_id, device_map="cpu", trust_remote_code=True)
